3b2618343a83114cb0da496ac610493e2216e5b9
kevinaw <kevinaw613@gmail.com>
2014-08-05 22:03:47 -0600

    add main widget and dockable widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QListWidget>
+#include <QDockWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // central widget
+    QTabWidget* mainWidget = new QTabWidget(this);
+    setCentralWidget(mainWidget);
+
+    // dockable widget
+    QDockWidget* docWidget = new QDockWidget(this);
+    QListWidget* listWidget = new QListWidget(this);
+    listWidget->setViewMode(QListView::IconMode);
+    listWidget->setGridSize(QSize(110, 90));
+    docWidget->setWidget(listWidget);
+    docWidget->setAllowedAreas(Qt::LeftDockWidgetArea);
+    docWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
+    docWidget->setWindowTitle("Components");
+    addDockWidget(Qt::LeftDockWidgetArea, docWidget);
+
 }
 
 MainWindow::~MainWindow()
